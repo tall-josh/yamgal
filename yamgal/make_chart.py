2113f66dbb633e74182d55c86961992710d95905
joshua patterson <joshua.patterson@silverpond.com.au>
2020-05-26 09:33:08 +1000

    url config

--- yamgal/make_chart.py
+++ yamgal/make_chart.py
@@ -2,62 +2,75 @@ import pygal
 import yaml
 
 CHART_FROM_NAME_STR = {
-    "Line" : pygal.Line,
-    "StackedLine": pygal.StackedLine,
-    "Bar": pygal.Bar,
-    "StackedBar": pygal.StackedBar,
-    "HorizontalBar": pygal.HorizontalBar,
-    "Pie": pygal.Pie,
-    "Histogram": pygal.Histogram,
-    "XY": pygal.XY
+    "line" : pygal.Line,
+    "stackedLine": pygal.StackedLine,
+    "bar": pygal.Bar,
+    "stackedbar": pygal.StackedBar,
+    "horizontalbar": pygal.HorizontalBar,
+    "pie": pygal.Pie,
+    "histogram": pygal.Histogram,
+    "xy": pygal.XY
 }
 
 STYLE_FROM_NAME_STR = {
-    "Default": pygal.style.DefaultStyle,
-    "Dark": pygal.style.DarkStyle,
-    "Neon": pygal.style.NeonStyle,
-    "DarkSolarized": pygal.style.DarkSolarizedStyle,
-    "LightSolarized": pygal.style.LightSolarizedStyle,
-    "Light": pygal.style.LightStyle,
-    "Clean": pygal.style.CleanStyle,
-    "RedBlue": pygal.style.RedBlueStyle,
-    "DarkColorized": pygal.style.DarkColorizedStyle,
-    "LightColorized": pygal.style.LightColorizedStyle,
-    "Turquoise": pygal.style.TurquoiseStyle,
-    "LightGreen": pygal.style.LightGreenStyle,
-    "DarkGreen": pygal.style.DarkGreenStyle,
-    "DarkGreenBlue": pygal.style.DarkGreenBlueStyle,
-    "Blue": pygal.style.BlueStyle,
+    "default": pygal.style.DefaultStyle,
+    "dark": pygal.style.DarkStyle,
+    "neon": pygal.style.NeonStyle,
+    "darksolarized": pygal.style.DarkSolarizedStyle,
+    "lightsolarized": pygal.style.LightSolarizedStyle,
+    "light": pygal.style.LightStyle,
+    "clean": pygal.style.CleanStyle,
+    "redblue": pygal.style.RedBlueStyle,
+    "darkcolorized": pygal.style.DarkColorizedStyle,
+    "lightcolorized": pygal.style.LightColorizedStyle,
+    "turquoise": pygal.style.TurquoiseStyle,
+    "lightgreen": pygal.style.LightGreenStyle,
+    "darkgreen": pygal.style.DarkGreenStyle,
+    "darkgreenblue": pygal.style.DarkGreenBlueStyle,
+    "blue": pygal.style.BlueStyle,
 }
 
 CHART_TYPE = 'chart_type'
 DATA = 'data'
 
-def validate_data(data):
-    if not(CHART_TYPE in data):
+def validate_chart_dict(chart_dict):
+    if not(CHART_TYPE in chart_dict):
         raise KeyError(f'"{CHART_TYPE}" must be specified')
-    if not(data[CHART_TYPE] in CHART_FROM_NAME_STR):
-        raise ValueError(f'Invalid chart_type: "{data[CHART_TYPE]}"')
-    if not(DATA in data):
+    if not(chart_dict[CHART_TYPE].lower() in CHART_FROM_NAME_STR):
+        raise ValueError(f'Invalid chart_type: "{chart_dict[CHART_TYPE]}"')
+    if not(DATA in chart_dict):
         raise KeyError('"data" MUST be specified')
 
+def get_chart_style(chart_dict):
+    style_name = chart_dict['chart_config'].get('style', 'default').lower()
+    return STYLE_FROM_NAME_STR[style_name]
 
-def make_chart(data):
+def get_chart_constructor(chart_dict):
+    chart_name = chart_dict['chart_type'].lower()
+    return CHART_FROM_NAME_STR[chart_name]
 
-    validate_data(data)
+def add_data_to_chart(chart, data):
+    for k, v in data.items():
+        chart.add(k, v)
+    return chart
 
-    style = data['chart_config'].get('style', 'Default')
-    data['chart_config']['style'] = STYLE_FROM_NAME_STR[style]
+def add_x_labels_to_chart(chart, chart_dict):
+    chart.x_labels = chart_dict.get('x_labels', None)
+    return chart
 
-    init_chart = CHART_FROM_NAME_STR[data['chart_type']]
-    chart = init_chart(**data['chart_config'],
-                       human_readable = True,
-                      )
+def add_y_labels_to_chart(chart, chart_dict):
+    chart.y_labels = chart_dict.get('y_labels', None)
+    return chart
 
-    for run, values in data['data'].items():
-        chart.add(run, values)
+def make_chart(chart_dict):
 
-    chart.x_labels = data.get('x_labels', None)
-    chart.y_labels = data.get('y_labels', None)
+    validate_chart_dict(chart_dict)
+
+    chart_dict['chart_config']['style'] = get_chart_style(chart_dict)
+    chart_constructor = get_chart_constructor(chart_dict)
+    chart = chart_constructor(**chart_dict['chart_config'])
+    chart = add_data_to_chart(chart, chart_dict['data'])
+    chart = add_x_labels_to_chart(chart, chart_dict)
+    chart = add_y_labels_to_chart(chart, chart_dict)
 
     return chart
